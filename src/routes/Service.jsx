9b6fc38926e649573c47e1a2c432e91c51740f06
ForestWatchAI <140009213+ForestWatchAI@users.noreply.github.com>
2023-07-28 15:13:41 +0530

    Update Service.jsx

--- src/routes/Service.jsx
+++ src/routes/Service.jsx
@@ -1,10 +1,15 @@
 import Navbar from "../Components/Navbar";
-
+import "./servicestyle.css"
 function Service (){
     return(
         <>
             <Navbar />
-            <h1>This is service</h1>
+            <h1>This is service page</h1>
+            <div id="ser-div">
+            <button className="ser-btn">this is first button</button>
+            <button className="ser-btn">this is second button</button>
+            <button className="ser-btn">this is third button</button>
+            </div>
         </>
     )
 }
