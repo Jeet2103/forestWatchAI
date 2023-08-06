a116dc9e806b670359738c480684218b786a5695
Rohit Kumar Dey <rohit.dey302003@gmail.com>
2023-08-06 19:45:40 +0530

    added poacher detection,forest fire alert and fload alert route to App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ import Service from "./routes/Service";
 import Contact from "./routes/Contact";
 import Login from "./routes/Login";
 import Signup from "./routes/Signup";
+import Poacher from "./routes/Poacher";
+import ForestFire from "./routes/ForestFire";
+import Flood from "./routes/Flood";
+import "./styles.css";
 
 
 export default function App() {
@@ -17,6 +21,9 @@ export default function App() {
         <Route path="/" element={<Home />}/>
         <Route path="/about" element={<About />}/>
         <Route path="/service" element={<Service />}/>
+        <Route path="/poacher detection" element={<Poacher />}/>
+        <Route path="/forest fire alert" element={<ForestFire />}/>
+        <Route path="/flood alert" element={<Flood />}/>
         <Route path="/contact" element={<Contact />}/>
         <Route path="/login" element={<Login />}/>
         <Route path="/signup" element={<Signup />}/>
